64f8585e9249e19720aa985ed3889d9c9d9fac49
Alec Hyde <alechyde91@gmail.com>
2018-07-10 12:34:25 -0700

    Implemented function to parse and format data from post request

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,15 @@
 
+// jquerry to grab the form element
+// on(trigger, callback)
+$(form).on('submit', (e) => {
+  e.preventDefault();
+  // make ajax request once submit button is clicked
+  $.ajax({
+    type: "POST",
+    url: 'http://127.0.0.1:3000/',
+    data: ,
+    success: console.log('successfully submitted'),
+  });
+  $.post('http://127.0.0.1:3000/', )
+});
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -4,3 +4,37 @@ var app = express();
 app.use(express.static('client'));
 app.listen(3000);
 
+
+var createCSVReport = function(json) {
+  // first parse back to an object
+  var obj = JSON.parse(json);
+  var CSVReport = ``;
+  var keys = Object.keys(obj);
+  // remove children attribute from keys
+  var child = keys.pop();
+  // add keys to CSV report
+  for (var i = 0; i < keys.length; i++) {
+    CSVReport += keys[i]; 
+    CSVReport += ',';
+  }
+  // add linebreak to CSV report 
+  CSVReport += '\n';
+  // recursively iterate through obj to add values to CSV report
+  var recurse = function (object) {
+    // base case: add values to CSV report and add linebreak at the end
+    for (var i = 0; i < keys.length; i++) {
+      CSVReport += object[keys[i]]; 
+      CSVReport += ',';
+    }
+    CSVReport += '\n';
+    // recursive case: check to see if children are present, if so, call recurse on children
+    if (object[child].length > 0) {
+      for (var j = 0; j < object[child].length; j++) {  
+        recurse(object[child][j]);
+      }
+    }
+  }
+  recurse(obj);
+}
+
+
